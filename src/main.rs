149cf09111f849dd4f1966f13a7921a7a5f81061
kotobukid <kotobuki@daqua.jp>
2025-04-14 17:28:20 +0900

    mpscをtokioのものに揃えてコンパイル成功

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use chrono::{DateTime, Local, NaiveDate, NaiveDateTime, Timelike};
+use once_cell::sync::OnceCell;
+use serde::Deserialize;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-use serenity::prelude::*;
 use serenity::model::id::ChannelId;
-use once_cell::sync::OnceCell;
+use serenity::prelude::*;
 use std::num::ParseIntError;
-use chrono::{Local, DateTime, NaiveDate, NaiveDateTime, Timelike};
+// use std::sync::mpsc;
+// use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use tokio::net::TcpListener;
+use tokio::runtime::Runtime;
+use tokio::sync::mpsc::{self, Receiver, Sender};
 
 static CHANNEL: OnceCell<u64> = OnceCell::new();
 
@@ -18,20 +30,25 @@ pub fn get_channel() -> Result<u64, ParseIntError> {
     })
 }
 
-struct Handler;
+struct Handler {
+    receiver: Receiver<String>,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!wp" {
-            let strings = Ok::<String, anyhow::Error>("good morning".to_string());
+            let strings: Result<String, String> = Result::Ok("test".to_string());
 
             match strings {
                 Ok(s) => {
-                    let res = msg.channel_id.say(&ctx.http, follow_cutting_string(s)).await;
+                    let res = msg
+                        .channel_id
+                        .say(&ctx.http, follow_cutting_string(s))
+                        .await;
                     match res {
                         Ok(_) => (),
-                        Err(e) => eprintln!("{:?}", e)
+                        Err(e) => eprintln!("{:?}", e),
                     }
                 }
                 _ => {
@@ -54,29 +71,38 @@ impl EventHandler for Handler {
                 let target_hour: u32 = 0;
                 let target_min: u32 = 5;
 
-                if now.time().hour() > target_hour || (now.time().hour() == target_hour && now.time().minute() >= target_min) {
+                if now.time().hour() > target_hour
+                    || (now.time().hour() == target_hour && now.time().minute() >= target_min)
+                {
                     // If it is past 00:05, set date to tomorrow. 地球が存在する限り失敗しない
                     date = date.succ_opt().expect("failed to get next date");
                 }
 
                 // このハードコーディングであれば失敗しない
-                let tomorrow_at_0005: NaiveDateTime = date.and_hms_opt(target_hour, target_min, 0).expect("failed to get time");
+                let tomorrow_at_0005: NaiveDateTime = date
+                    .and_hms_opt(target_hour, target_min, 0)
+                    .expect("failed to get time");
                 // println!("{}", tomorrow_at_0005.format("%F %R"));
 
                 let duration_to_wait = tomorrow_at_0005.signed_duration_since(now.naive_local());
 
-                println!("next action at after {} min", duration_to_wait.num_minutes());
+                println!(
+                    "next action at after {} min",
+                    duration_to_wait.num_minutes()
+                );
 
                 tokio::time::sleep(duration_to_wait.to_std().unwrap()).await;
 
-                let strings = Ok::<std::string::String, anyhow::Error>("hello".to_string());
+                let strings: Result<String, String> = Ok("test".to_string());
 
                 match strings {
                     Ok(s) => {
-                        let res = channel_id.say(&context.http, follow_cutting_string(s)).await;
+                        let res = channel_id
+                            .say(&context.http, follow_cutting_string(s))
+                            .await;
                         match res {
                             Ok(_) => (),
-                            Err(e) => eprintln!("{:?}", e)
+                            Err(e) => eprintln!("{:?}", e),
                         }
                     }
                     Err(e) => {
@@ -120,12 +146,11 @@ fn follow_cutting_string(string: String) -> String {
     let string_cut = get_first_n_characters(&string, 1900);
     match string_cut {
         CutStringResult::Pass(s) => s,
-        CutStringResult::Cut(s) => format!("{}\n(省略されました)", s)
+        CutStringResult::Cut(s) => format!("{}\n(省略されました)", s),
     }
 }
 
-#[tokio::main]
-async fn main() {
+async fn run_discord_bot(rx: Receiver<String>) {
     match dotenv::dotenv() {
         Ok(_) => (),
         Err(error) => eprintln!("Couldn't read .env file: {:?}", error),
@@ -140,10 +165,74 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler { receiver: rx })
+        .await
+        .expect("Err creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct HookMessage {
+    message: String,
+}
+
+async fn webhook_handler(
+    State(sender): State<Sender<String>>,
+    Json(payload): Json<HookMessage>,
+) -> impl IntoResponse {
+    println!("webhook_handler: {:?}", payload);
+    let sender = sender.clone();
+
+    if let Err(e) = sender.send(payload.message.clone()).await {
+        eprintln!("メッセージの送信に失敗: {}", e);
+    }
+
+    (StatusCode::OK, "OK")
+}
+
+async fn get_index_handler() -> &'static str {
+    "Hello, world!"
+}
+
+async fn run_web_server(tx: Sender<String>) {
+    // Axumルーターの設定
+    let app = Router::new()
+        .route("/webhook", post(webhook_handler))
+        .route("/", get(get_index_handler))
+        .with_state(tx.clone());
+
+    // サーバーを起動
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("Webサーバーは {} で起動中...", addr);
+    let socket: std::io::Result<TcpListener> = TcpListener::bind(&addr).await;
+    let listener: TcpListener = socket.expect("Failed to bind socket");
+    let _ = axum::serve(listener, app.into_make_service()).await;
+}
+
+#[tokio::main]
+async fn main() {
+    // mpscのSenderとReceiverを作成
+    let (tx, rx) = mpsc::channel::<String>(32);
+
+    // Webサーバーを動かすスレッドの作成
+    let web_server_thread = thread::spawn(move || {
+        // tokioランタイムを生成
+        let rt = Runtime::new().unwrap();
+        // ランタイム内でAxumを非同期実行
+        rt.block_on(run_web_server(tx));
+    });
+
+    let discord_bot_thread = thread::spawn(move || {
+        // tokioランタイムを生成
+        let rt = Runtime::new().unwrap();
+        // ランタイム内でSerenityを非同期実行
+        rt.block_on(run_discord_bot(rx));
+    });
+
+    discord_bot_thread.join().unwrap();
+    web_server_thread.join().unwrap();
 }
